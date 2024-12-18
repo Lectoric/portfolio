70c1302367a4087b40d5ba6396ab700f9d3e247c
PanayiotisEvangelou233096 <panayiotis.kiti@gmail.com>
2024-12-18 16:41:00 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
-// filepath: /c:/Users/panay/Desktop/BUAS/YEAR 2/REPOSITORIES/portfolio/src/index.jsximport React from 'react';
+// filepath: /c:/Users/panay/Desktop/BUAS/YEAR 2/REPOSITORIES/portfolio/src/index.jsx
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
