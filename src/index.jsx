b26000e2a1067c28d8e9833d65b4165d99cd2734
PanayiotisEvangelou233096 <panayiotis.kiti@gmail.com>
2024-12-18 16:38:10 +0100

    a

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// filepath: /c:/Users/panay/Desktop/BUAS/YEAR 2/REPOSITORIES/portfolio/src/index.jsximport React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
