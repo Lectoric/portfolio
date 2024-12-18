258b4872eacca2985962aeefef5ad5f5e18c18af
PanayiotisEvangelou233096 <panayiotis.kiti@gmail.com>
2024-12-18 14:43:46 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-// filepath: /c:/Users/panay/Desktop/BUAS/YEAR 2/REPOSITORIES/portfolio/src/App.jsx
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Navbar from './components/navbar';
 import Projects from './pages/projects';
@@ -8,15 +7,17 @@ import About from './pages/about';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-        <HashRouter>
-          <Routes>
-            <Route path="/" element={<Projects />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </HashRouter>
-       </div>
+    <Router basename="/portfolio">
+      <div>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
 
